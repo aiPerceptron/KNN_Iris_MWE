619a14e4d5cb8d69cbd77c7f6365f8cfa75a2573
aiPerceptron <193553617+aiPerceptron@users.noreply.github.com>
2025-01-16 18:44:09 -0800

    Update KNN_Iris_MWE.py

--- KNN_Iris_MWE.py
+++ KNN_Iris_MWE.py
@@ -18,10 +18,10 @@ iris = load_iris(return_X_y=True, as_frame=True)
 #    print(key)
 # print(type(iris["frame"]))
 
-X = np.array(iris[0])
-y = np.array(iris[1])
+X = np.array(iris[0]) # X is the features
+y = np.array(iris[1]) # y is the targets/labels
 
-X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.25)
+X_train,X_test,y_train,y_test = train_test_split(X, y, test_size=0.25, shuffle=True)
 
 # step two: picking the algorithm
 algo = KNeighborsClassifier()
@@ -39,20 +39,32 @@ y_pred = algo.predict(X_test)
 accurate_score = round(accuracy_score(y_test, y_pred)*100, 2) 
 print(str(accurate_score) + "% accuracy")
 
-sepal_length = X[:, 0]
-sepal_width = X[:, 1]
-petal_length = X[:, 2]
-petal_width = X[:, 3]
+sepal_length_train = X_train[:, 0] # ALL of the rows but only the first column
+sepal_width_train = X_train[:, 1] # ALL of the rows but only the second column
+petal_length_train = X_train[:, 2] # ALL of the rows but only the third column
+petal_width_train = X_train[:, 3] # ALL of the rows but only the fourth column
 
-plt.scatter(sepal_length, sepal_width, c=y, s=15)
+sepal_length_test = X_test[:, 0]
+sepal_width_test = X_test[:, 1]
+petal_length_test = X_test[:, 2]
+petal_width_test = X_test[:, 3]
+
+# print(X_test[:, 0]) # selects the 0th column
+# print(X_test[0, :]) # this one selects the 0th row
+# print(X_test[0]) # this one defaults to selecting the 0th row
+
+plt.scatter(sepal_length_train, sepal_width_train, c=y_train, s=15)
 plt.title("Sepals")
 plt.xlabel("Sepal Length (cm)")
 plt.ylabel("Sepal Width (cm)")
+plt.scatter(sepal_length_test, sepal_width_test, s=20, marker="x",c=y_test)
 plt.savefig("IrisKNN_SKlearn_Sepals.png")
+
 plt.figure()
-plt.scatter(petal_length, petal_width, c=y, s=15)
+plt.scatter(petal_length_train, petal_width_train, c=y_train, s=15)
 plt.title("Petals")
 plt.xlabel("Petal Length (cm)")
 plt.ylabel("Petal Width (cm)")
+plt.scatter(petal_length_test, petal_width_test, s=20, marker="x",c=y_test)
 plt.savefig("IrisKNN_SKlearn_Petals.png")
 # print(pd.Series(iris))
